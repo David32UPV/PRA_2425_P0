1ef49c126e2f86efb33f5cdd9dc0f1b739a048dc
David Bernad López <dberlop@etsinf.upv.es>
2024-09-29 23:05:33 +0200

    Brazo Robótico .h inicios

---
+++ BrazoRobotico.h
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class BrazoRobotico{
+
+	private:
+
+		// Coordenadas x, y, z del espacio 3D
+		int x, y, z;
+		bool sujetandoObjeto;
+	
+	public:
+		
+		// Constuctor
+		BrazoRobotico();
+				
+		// Consultores(getters) para los 4 atributos privados (i.e. 4 consultores)
+		
+
+};
+
